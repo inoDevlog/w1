c3d290e170b4a8aa187227adbcd7be06c7fbe69d
choiinho <ino0319@gmail.com>
2023-01-04 22:28:28 +0900

    1장. 웹 프로그래밍의 시작

--- src/main/java/com/ino/w1/todo/TodoReadController.java
+++ src/main/java/com/ino/w1/todo/TodoReadController.java
@@ -1,2 +1,28 @@
-package com.ino.w1.todo;public class TodoReadController {
+package com.ino.w1.todo;
+
+import com.ino.w1.todo.dto.TodoDTO;
+import com.ino.w1.todo.service.TodoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoReadController", urlPatterns = "/todo/read")
+public class TodoReadController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("/todo/read");
+
+        // /todo/read?tno-123
+        long tno = Long.parseLong(req.getParameter("tno"));
+        TodoDTO dto = TodoService.INSTANCE.get(tno);
+        req.setAttribute("dto", dto);
+        req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
+
+    }
 }
--- src/main/java/com/ino/w1/todo/service/TodoService.java
+++ src/main/java/com/ino/w1/todo/service/TodoService.java
@@ -1,2 +1,40 @@
-package com.ino.w1.todo.service;public class TodoService {
+package com.ino.w1.todo.service;
+
+import com.ino.w1.todo.TodoListController;
+import com.ino.w1.todo.dto.TodoDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public enum TodoService {
+    INSTANCE,
+    ;
+
+    public void register(TodoDTO todoDTO) {
+        System.out.println("DEBUG.........." + todoDTO);
+    }
+
+    public List<TodoDTO> getList() {
+        List<TodoDTO> todoDTOS = IntStream.range(0, 10).mapToObj(i -> {
+            TodoDTO dto = new TodoDTO();
+            dto.setTno((long) i);
+            dto.setTitle("Todo.." + i);
+            dto.setDueDate(LocalDate.now());
+            return dto;
+        }).collect(Collectors.toList());
+
+        return todoDTOS;
+    }
+
+    public TodoDTO get(Long tno) {
+        TodoDTO dto = new TodoDTO();
+        dto.setTno(tno);
+        dto.setTitle("Sample todo");
+        dto.setDueDate(LocalDate.now());
+        dto.setFinished(true);
+
+        return dto;
+    }
 }
